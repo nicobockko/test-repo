848df9c509d38b6a6f78e00d9d1ea9440369019f
nicobockko <cjs910955@gmailcom>
2024-06-21 21:49:17 +0900

    d

--- 튜토리얼/csv파일다루기/n.py
+++ 튜토리얼/csv파일다루기/n.py
@@ -1,5 +1,7 @@
 # git filter-repo --path 튜토리얼/csv파일다루기/10000000.csv --invert-paths --force
 
-# git remote add origin https://github.com/nicobockko/mysite.git
+# git remote add origin https://github.com/nicobockko/test-repo.git
+
+#git remote rm origin
 
 #
